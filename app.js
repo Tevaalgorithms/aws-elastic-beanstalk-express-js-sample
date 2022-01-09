ab2d767f50030f80fda989ac69000f49c295c0ac
Tevaganthan Veluppillai <45029995+Tevaalgorithms@users.noreply.github.com>
2022-01-09 11:12:58 -0700

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello Teva's World!'));
+app.get('/', (req, res) => res.send('Hello Teva Pipeline World!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
